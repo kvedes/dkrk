8f9640055533bab799eed21cb3e3cd915e38966a
matkvist <bejsebakkevej@hotmail.com>
2023-10-24 13:51:20 +0200

    Add Maturity class

--- dkrk/realestate.py
+++ dkrk/realestate.py
@@ -2,6 +2,7 @@
 Module handling calculations related to Danish Real Estate Loans
 """
 from abc import ABC, abstractmethod
+from enum import Enum
 from dkrk.annuity import Annuity
 import pandas as pd
 import numpy as np
@@ -34,6 +35,54 @@ class Quarter(Term):
             self.start + relativedelta(months=3), self.end + relativedelta(months=3)
         )
 
+class TKDueDate(Enum):
+    march = (3, 31)
+    june = (6, 30)
+    september = (9, 30)
+    december = (12, 31)
+
+
+
+class Maturity:
+
+    def __init__(self, time_to_maturity: relativedelta, date: datetime):
+        """Class containing information about current date and time to maturity.
+        Current date doesnt have to be the day the bond is issued.
+
+        Parameters
+        ----------
+        time_to_maturity : relativedelta
+            The time until the final payment is made on the loan/bond (maturity)
+        date : datetime
+            The current date
+        """
+        self.time_to_maturity = time_to_maturity
+        self.date = date
+
+    @staticmethod
+    def next_due_date(date: datetime) -> datetime:
+        cur_year = date.year
+        for due_month in TKDueMonth:
+            loop_due = datetime(cur_year, due_month.value[0], due_month.value[1])
+            if date < loop_due:
+                break
+        
+        return loop_due 
+
+    def due_dates(self) -> List[datetime]:
+        res = []
+        cur_date = self.date.copy()
+        end_date = self.date + self.time_to_maturity
+        while cur_date <= end_date:
+            tmp_due = Maturity.next_due_date(cur_date)
+            if tmp_due <= end_date:
+                res.append(
+                    tmp_due
+                )
+            else:
+                break
+        
+        return res
 
 class TKLoan:
     """
@@ -42,6 +91,47 @@ class TKLoan:
 
     def __init__(
         self,
+        face_value: float,
+        interest: float,
+        bidrag: float,
+        maturity: int = 30,
+        n_terms: int = 4,
+    ):
+        """
+        Parameters
+        ----------
+        face_value : float
+            The size of the loan
+        interest : float
+            The interest rate of the loan, as a decimal. E.g. 5% would be 0.05
+        bidrag : float
+            Bidrag is interest paid to the bank. Should be annual interest.
+            #TODO: Find english word for bidrag
+        maturity : int
+            The time to maturity. E.g. 30 for 30 years
+        n_terms : int
+            The number of payments made for each term. E.g. if the loan is for 30 years
+            and payments are made each quarter, then n_paymens should 4. NB: It is not
+            the total number of payments made during the lifetime of the annuity!
+
+        """
+        self.face_value = face_value
+        self.interest = interest
+        self.bidrag = bidrag
+        self.maturity = maturity
+        self.n_terms = n_terms
+
+        # Calc annuity
+        self.annuity_obj = Annuity(
+            self.face_value, self.interest, self.maturity, self.n_terms
+        )
+        self.table = self._add_bidrag(self.annuity_obj.table, self.bidrag, self.n_terms)
+
+        # Cash flow
+        self.cash_flow = [-self.face_value] + self.table["Annuity"].values.tolist()
+    
+    @classmethod
+    def from_price(cls,
         price: float,
         face_value: float,
         interest: float,
@@ -50,6 +140,10 @@ class TKLoan:
         n_terms: int = 4,
     ):
         """
+        Create a new loan with a desired size. E.g. you want to borrow 1000000
+        and the bond is priced at 95. Thus you need a bond with face value
+        1000000/0.95 = 1052631
+
         Parameters
         ----------
         price : float
@@ -69,23 +163,19 @@ class TKLoan:
             the total number of payments made during the lifetime of the annuity!
 
         """
-        self.price = price
-        self.face_value = face_value
-        self.interest = interest
-        self.bidrag = bidrag
-        self.maturity = maturity
-        self.n_terms = n_terms
+
 
         # Adjust loan amount by bond price
-        self.loan_amount = self.face_value / (self.price / 100)
-        self.annuity_obj = Annuity(
-            self.loan_amount, self.interest, self.maturity, self.n_terms
-        )
-        self.table = self._add_bidrag(self.annuity_obj.table, self.bidrag, self.n_terms)
+        loan_amount = face_value / (price / 100)
 
-        # Cash flow
-        self.cash_flow = [-self.face_value] + self.table["Annuity"].values.tolist()
-    
+        return cls(
+            loan_amount,
+            interest,
+            bidrag,
+            maturity,
+            n_terms
+        )
+
     @staticmethod
     def _add_bidrag(table: pd.DataFrame, bidrag: float, n_terms: int) -> pd.DataFrame:
         """
